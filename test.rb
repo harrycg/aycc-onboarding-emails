b26d13146add187f4297fb5e8881a777f904fc8e
harrycg <39111936+harrycg@users.noreply.github.com>
2019-02-01 10:42:25 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 
 require 'nationbuilder'
 
-client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
+client = NationBuilder::Client.new('harrysandboxdev', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
 puts "find recently created people"
 
