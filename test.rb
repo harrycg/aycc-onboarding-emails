815a694801176b47d2b2290649878169137fe96f
harrycg <39111936+harrycg@users.noreply.github.com>
2019-02-08 13:51:25 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -23,6 +23,38 @@ while create_recently_2.next?
   create_recently_3 += create_recently_2.body['results']
 end  
 
+#gets email and id from the list of people to then add membersip
+create_recently_3.each do |create_recently_4|
+  
+  #will just add membership to people who's profile was created in the last day
+  if Date.parse(create_recently_4['created_at']) >= yesterday_1  
+  
+  email = create_recently_4['email']
+  id = create_recently_4['id']
+  tags = create_recently_4['tags']
+  puts "#{email} id #{id} why #{tags}" 
+  
+  
+  end  
+    
+=begin
+#sets the tag you want to pull out
+create_recently_tag = {
+  tag: "harrytest123"
+  }
+  
+#pulls people from nationbuilder
+create_recently_1 = client.call(:people_tags, :people, create_recently_tag)
+create_recently_2 = NationBuilder::Paginator.new(client, create_recently_1)
+
+#stores results
+create_recently_3 = []
+  create_recently_3 += create_recently_2.body['results']
+while create_recently_2.next?
+  create_recently_2 = create_recently_2.next
+  create_recently_3 += create_recently_2.body['results']
+end  
+
 
 yesterday_1 =  DateTime.now - 1
 
